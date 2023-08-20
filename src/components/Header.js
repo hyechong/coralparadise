a3f670e499048e75bd4988a1a510c91dd1d9e5e9
hyechong <ckgpwl2@gmail.com>
2023-08-20 18:11:08 +0900

    header 0820

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { NavList } from '../styles/HeaderStyle';
+import { styled } from 'styled-components';
+import logo from '../assets/logo.png';
+import { Container } from '../styles/CommonStyles';
+
+const NavList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: auto;
+  padding: 1.25rem 0;
+
+  ul {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+  }
+  & > .logo-wrapper {
+    width: 200px;
+  }
+  & > .logo-wrapper > img {
+    width: 100%;
+  }
+`;
+const LogoLink = styled(Link)`
+  width: 200px;
+  & > img {
+    width: 100%;
+  }
+`;
+const ShadowHeader = styled(Container)`
+  box-shadow: 1px 1px 5px #efefef;
+`;
 
 const Header = () => {
   return (
-    <div>
-      <NavList>
-        <ul className='navi'>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/search-lists'>SearchLists</Link>
-          </li>
-        </ul>
-      </NavList>
-    </div>
+    <header>
+      <ShadowHeader>
+        <NavList>
+          <LogoLink to='/'>
+            <img src={logo} alt='logo' />
+          </LogoLink>
+          <ul className='navi'>
+            <li>
+              <Link to='/'>로그인</Link>
+            </li>
+            <li>
+              <Link to='/wish-lists'>위시리스트</Link>
+            </li>
+          </ul>
+        </NavList>
+      </ShadowHeader>
+    </header>
   );
 };
 
