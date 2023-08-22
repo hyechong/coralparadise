7c42d634d88d7dc21db191d2abfe519d18676e6c
hyechong <ckgpwl2@gmail.com>
2023-08-23 01:55:46 +0900

    searchlist 0823

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import SearchInput from '../components/SearchInput';
+import { Container, Section } from '../styles/CommonStyles';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Container>
+        <Section>
+          <SearchInput />
+        </Section>
+      </Container>
     </div>
   );
 };
--- src/pages/SearchLists.js
+++ src/pages/SearchLists.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { fetchData, getOptions } from '../utils/fetchData';
 import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import Header from '../components/Header';
+import SearchItems from '../components/SearchItems';
 
 const SearchLists = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = searchParams.get('location');
+  const checkIn = searchParams.get('checkIn');
+  const checkOut = searchParams.get('checkOut');
   useEffect(() => {
     const getSearchData = async () => {
       const getData = await fetchData(
-        'https://airbnb13.p.rapidapi.com/search-location?location=Paris&checkin=2023-09-16&checkout=2023-09-17&adults=1&children=0&infants=0&pets=0&page=1&currency=USD',
+        `https://airbnb13.p.rapidapi.com/search-location?location=${location}&checkin=${checkIn}&checkout=${checkOut}&adults=1&children=0&infants=0&pets=0&page=1&currency=USD`,
         getOptions
       );
       console.log(getData);
+      return getData;
     };
 
-    // getSearchData();
+    getSearchData();
   }, []);
 
-  return <div>SearchLists</div>;
+  return (
+    <div>
+      <Header />
+      <SearchItems />
+    </div>
+  );
 };
 
 export default SearchLists;
