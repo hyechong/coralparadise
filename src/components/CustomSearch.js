bd672dcb588a526958ed19d34e64f5cbd651b35c
hyechong <ckgpwl2@gmail.com>
2023-09-05 20:22:00 +0900

    googlemap0905

--- src/components/CustomSearch.js
+++ src/components/CustomSearch.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import { Container } from '../styles/CommonStyles';
+import { fetchData, getOptions } from '../utils/fetchData';
+import { getFormattedTodayDate, getFormattedTomorrowDate } from '../utils/util';
 import Button from './Button';
 
 const CustomSearchWrapper = styled.div`
@@ -29,14 +31,81 @@ const CustomSearchWrapper = styled.div`
 `;
 
 const CustomSearch = () => {
-  const [options, setOptions] = useState({});
+  const [options, setOptions] = useState({
+    loca: 'America',
+    adults: '1',
+    children: '1',
+    pets: '0',
+    checkIn: getFormattedTodayDate(new Date()),
+    checkOut: getFormattedTomorrowDate(new Date()),
+  });
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    if (value === 'child') {
+      setOptions({
+        ...options,
+        adults: '1',
+        children: '1',
+        pets: '0',
+      });
+    } else if (value === 'alone') {
+      setOptions({
+        ...options,
+        adults: '1',
+        children: '0',
+        pets: '0',
+      });
+    } else if (value === 'couple') {
+      setOptions({
+        ...options,
+        adults: '2',
+        children: '0',
+        pets: '0',
+      });
+    } else if (value === 'pet') {
+      setOptions({
+        ...options,
+        adults: '1',
+        children: '0',
+        pets: '1',
+      });
+    } else {
+      setOptions({
+        ...options,
+        [name]: value,
+      });
+    }
+  };
+
+  const [customResults, setcustomResults] = useState([]);
+
+  // console.log(options);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+  const getCustomData = async () => {
+    const getData = await fetchData(
+      `https://airbnb13.p.rapidapi.com/search-location?location=${options.loca}&checkin=${options.checkIn}&checkout=${options.checkOut}&adults=${options.adults}&children=${options.children}&infants=0&pets=${options.pets}&page=1&currency=KRW`,
+      getOptions
+    );
+
+    setcustomResults(getData.results);
+  };
+  useEffect(() => {
+    // getCustomData();
+  }, [handleOnSubmit]);
+  console.log(customResults);
+
   return (
     <CustomSearchWrapper id='custom-search' className='section'>
       <Container>
-        <form className='text-wrapper'>
+        <form className='text-wrapper' onSubmit={handleOnSubmit}>
           <h3>
             나는
-            <select>
+            <select
+              onChange={handleOnChange}
+              name='guest'
+              value={options.guest}>
               <option key='child' value='child'>
                 👶 아이와 함께
               </option>
@@ -46,12 +115,12 @@ const CustomSearch = () => {
               <option key='couple' value='couple'>
                 👩‍❤️‍👨연인과 함께
               </option>
-              <option key='dog' value='dog'>
+              <option key='pet' value='pet'>
                 🐶 반려동물과 함께
               </option>
             </select>
             <br />
-            <select>
+            <select onChange={handleOnChange} name='loca' value={options.loca}>
               <option key='America' value='America'>
                 🇺🇸 미국으로
               </option>
@@ -61,17 +130,17 @@ const CustomSearch = () => {
               <option key='Japan' value='Japan'>
                 🏙️ 일본으로
               </option>
-              <option key='Europe' value='Europe'>
+              <option key='Europe' value='Japan'>
                 🇪🇺 유럽으로
               </option>
-              <option key='Guam' value='Guam'>
+              <option key='Guam' value='Japan'>
                 🏝️ 괌으로
               </option>
             </select>
             떠나고 싶어요
           </h3>
           <div className='buttons'>
-            <Button text='최신순' mode='sub-white' />
+            <Button type='submit' text='최신순' mode='sub-white' />
             <Button text='리뷰순' mode='sub-point' />
           </div>
         </form>
